ed655dc98c01a198732238f7e215b0ade5a0be0c
mogr20 <markjordanrandall@gmail.com>
2025-03-27 15:23:01 +0000

    FIX: Debug false

--- loch_inn/consumers.py
+++ loch_inn/consumers.py
@@ -12,8 +12,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-
-        await self.channel.name.group_discard(
+        await self.channel_layer.group_discard(
             self.roomGroupName,
             self.channel_name
         )
