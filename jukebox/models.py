2f3374c43c9917d649abee6938d1aab8eb642f87
mogr20 <markjordanrandall@gmail.com>
2025-03-27 13:57:57 +0000

    ADD: music and list models

--- jukebox/models.py
+++ jukebox/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Music_Embed(models.Model):
+    """
+    
+    """
+    embed_code = models.CharField(max_length=255, unique=True)
+    genre = models.CharField(max_length=100, blank=True)
+    song_name = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    album_name = models.CharField(max_length=100, blank=True)
+    published_date = models.DateField()
+
+
+class Global_List(models.Model):
+    """
+    
+    """
+    embed_id = models.ForeignKey(
+        Music_Embed,
+        on_delete=models.CASCADE
+    )
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    datetime_added = models.DateTimeField(auto_now_add=True)
+
+
+class Local_List(models.Model):
+    """
+    
+    """
+    embed_id = models.ForeignKey(
+        Music_Embed,
+        on_delete=models.CASCADE
+    )
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    datetime_added = models.DateTimeField(auto_now_add=True)
+
+
