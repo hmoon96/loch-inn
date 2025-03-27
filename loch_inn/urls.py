4822bb71bda4591ad23033db25a5a34d1a3fb126
mogr20 <markjordanrandall@gmail.com>
2025-03-27 11:28:17 +0000

    ADD: allauth functionality, and base.html for template inheritence of nav

--- loch_inn/urls.py
+++ loch_inn/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('jukebox.urls'), name='home'),
+    path('accounts/', include('allauth.urls')),
+    path('', include('jukebox.urls'), name='jukebox-urls'),
 ]
