d153cc11e9d074b1a45cc6f1572a375207cb8b18
hmoon96 <hlmooney98@gmail.com>
2025-03-27 17:46:46 +0000

    added views to views.py for adding to local list and searching the database for a song. Added urls for the local list and search, added searchbar and field to list the song to the html. This isn't working properly yet, as the search won't link to the db properly

--- jukebox/urls.py
+++ jukebox/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.HomePage.as_view(), name="home"),
+    path("", views.search, name="index"),
+    path("search/<str:query>/", views.search, name="search"),
+    path('add-to-local-list/<int:embed_id>/', views.add_to_local_list, name='add_to_local_list'),
 ]
